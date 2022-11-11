fefef87f9a3fb64515e2cd724546dd0e0ad7c7d6
angelardhawaii <angelard@hawaii.edu>
2022-11-10 14:20:44 -1000

    Updated all in script and markdown file with the new dataset and analysis of ek at different days during experiment vs. irradiance (rather than comparison with just one value of Ek as before).

--- ek_irrad_model_script.R
+++ ek_irrad_model_script.R
@@ -26,7 +26,7 @@ library(purrr)
 library(stringr)
 library(tidyr)
 
-ek_irrad_data <- read.csv("/Users/Angela/src/work/limu/irradiance_ek/output/irrad_ek.csv")
+ek_irrad_data <- read.csv("input_data/mean_supersaturation_by_period.csv")
 
 # assign run as a factor
 ek_irrad_data$Run <- as.factor(ek_irrad_data$Run)
@@ -38,30 +38,35 @@ ek_irrad_data$Temperature <- as.factor(ek_irrad_data$Temp...C.)
 ek_irrad_data$Treatment <- as.factor(as.character(ek_irrad_data$Treatment))
 
 # assign deltaNPQ as a factor
-ek_irrad_data$deltaNPQ <- as.factor(ek_irrad_data$deltaNPQ)
+#ek_irrad_data$deltaNPQ <- as.factor(ek_irrad_data$deltaNPQ)
 
 #toggle between the species for output. Use Day 9 for final analysis
 #recent change: removing the very odd ek.1 value of 559.4 in hypnea dataset
-hypnea <- subset(ek_irrad_data, Species == "hm" & RLC.Day == 9 & ek.1 < 226)
-hypnea$treatment_graph[hypnea$Treatment == 1] <- "1) 35ppt/14umol" 
-hypnea$treatment_graph[hypnea$Treatment == 2] <- "2) 28ppt/27umol" 
-hypnea$treatment_graph[hypnea$Treatment == 3] <- "4) 18ppt/53umol" 
-hypnea$treatment_graph[hypnea$Treatment == 4] <- "5) 11ppt/80umol"
-hypnea$treatment_graph[hypnea$Treatment == 5] <- "3) 28ppt/53umol"
+hypnea <- subset(ek_irrad_data, Species == "hm")
+hypnea$treatment_graph[hypnea$Treatment == 0] <- "1) 35ppt/0.5umol"
+hypnea$treatment_graph[hypnea$Treatment == 1] <- "2) 35ppt/14umol" 
+hypnea$treatment_graph[hypnea$Treatment == 2] <- "3) 28ppt/27umol" 
+hypnea$treatment_graph[hypnea$Treatment == 3] <- "5) 18ppt/53umol" 
+hypnea$treatment_graph[hypnea$Treatment == 4] <- "6) 11ppt/80umol"
+hypnea$treatment_graph[hypnea$Treatment == 2.5] <- "4) 28ppt/53umol"
 
-ulva <- subset(ek_irrad_data, Species == "ul" & RLC.Day == 9)
+ulva <- subset(ek_irrad_data, Species == "ul")
 ulva$treatment_graph[ulva$Treatment == 0] <- "1) 35ppt/0.5umol"
 ulva$treatment_graph[ulva$Treatment == 1] <- "2) 35ppt/14umol" 
 ulva$treatment_graph[ulva$Treatment == 2] <- "3) 28ppt/27umol" 
-ulva$treatment_graph[ulva$Treatment == 3] <- "4) 18ppt/53umol" 
-ulva$treatment_graph[ulva$Treatment == 4] <- "5) 11ppt/80umol"
+ulva$treatment_graph[ulva$Treatment == 3] <- "5) 18ppt/53umol" 
+ulva$treatment_graph[ulva$Treatment == 4] <- "6) 11ppt/80umol"
+ulva$treatment_graph[ulva$Treatment == 2.5] <- "4) 28ppt/53umol"
+
 #add growth rate from other dataset to this one and subset by species
-growth_rate <- read.csv("/Users/Angela/src/work/limu/algal_growth_photosynthesis/data_input/run5-6_growth_all_042922.csv")
+growth_rate <- read.csv("/Users/Angela/src/work/limu/algal_growth_photosynthesis/data_input/all_runs_growth_102222.csv")
 growth_rate$Species <- as.factor(growth_rate$Species)
 growth_rate$treatment <- as.factor(growth_rate$treatment)
-growth_rate
+
 gr_ulva <- subset(growth_rate, Species == "Ul")
-gr_hypnea <- subset(growth_rate, Species == "Hm" & final.weight != 0.1017 & final.weight != 0.3224)
+
+#for Hypnea remove the same replicate that was removed in the previous dataset (hm6-4 on 11/12 that had no d9 RLC) 
+gr_hypnea <- subset(growth_rate, Species == "Hm" & final.weight != 0.1017)
 
 ulva$growth_rate <- round((gr_ulva$final.weight - gr_ulva$Initial.weight) / gr_ulva$Initial.weight * 100, digits = 2)
 hypnea$growth_rate <- round((gr_hypnea$final.weight - gr_hypnea$Initial.weight) / gr_hypnea$Initial.weight * 100, digits = 2)
@@ -69,112 +74,122 @@ hypnea$growth_rate <- round((gr_hypnea$final.weight - gr_hypnea$Initial.weight) 
 
 #____________________________________________________________
 #ULVA 
-#run model without interaction between the treatments and temperature - irradiance_over_ek is in minutes
+#run model without interaction between the treatments and temperature - supersat_total is in minutes
 #take RLC.Order our of the random effects because causing problems of singularity. R2 is same with or without (+ (1 | RLC.Order))
-ek_irrad_model_noint_ulva <- lmer(formula = irradiance_over_ek ~ Treatment + Temperature + (1 | Run) + (1 | Plant.ID), data = ulva)
+ek_irrad_model_ulva <- lmer(formula = supersat_total ~ Treatment + Temperature + (1 | Run) + (1 | Plant.ID), data = ulva)
 
 #make a histogram and residual plots of the data for ulva
-hist(ulva$irradiance_over_ek, main = paste("Ulva lactuca"), col = "olivedrab3", labels = TRUE)
-hist(resid(ek_irrad_model_noint))
-plot(resid(ek_irrad_model_noint) ~ fitted(ek_irrad_model_noint))
-qqnorm(resid(ek_irrad_model_noint))
-qqline(resid(ek_irrad_model_noint))
-
-
-#check the performance of the model
-performance::check_model(ek_irrad_model_noint)
-
-r.squaredGLMM(ek_irrad_model_noint)
-summary(ek_irrad_model_noint)
-
-#check for equal variance
-bartlett.test(irradiance_over_ek ~ Treatment, data = ulva)
-#run Welch's ANOVA if not equal variance
-welch_anova_treatment_ulva <- oneway.test(irradiance_over_ek ~ Treatment, data = ulva, var.equal = FALSE)
-welch_anova_treatment_ulva
-welch_anova_temp_ulva <- oneway.test(irradiance_over_ek ~ Temperature, data = ulva, var.equal = FALSE)
-welch_anova_temp_ulva
-games_howell_test(ulva, irradiance_over_ek ~ Treatment, conf.level = 0.95, detailed = TRUE)
-#run ANOVA and pairwise comparisons
-anova(ek_irrad_model_noint, type = c("III"), ddf = "Satterthwaite")
-ulva_ek_irrad_model_aov <- aov(irradiance_over_ek ~ Treatment + Temperature, data = ulva)
-TukeyHSD(ulva_ek_irrad_model_aov, "Treatment", ordered = FALSE)
-TukeyHSD(ulva_ek_irrad_model_aov, "Temperature", ordered = FALSE)
-
-plot(allEffects(ek_irrad_model_noint_ulva))
-
-#plot a regression between the photosynthetic independent variables of interest and growth rate
-ulva_growth_irrad_ek_graph <- ggplot(ulva, aes(x=irradiance_over_ek, y=growth_rate)) + 
-        geom_point(alpha = 0.5, size = 3, aes(color = Treatment), show.legend = TRUE) + 
-        geom_smooth(method = "lm", col = "black") + theme_bw() + 
-        labs(title = "Ulva lactuca Minutes above Ek vs Growth Rate", x = "Minutes above Ek", 
-             y = "growth rate (%)") + stat_regline_equation(label.x = 25, label.y = 165) + stat_cor()
-ulva_growth_irrad_ek_graph
-
-ulva %>% ggplot(aes(irradiance_over_ek)) +
+hist(ulva$supersat_total, main = paste("Ulva lactuca"), col = "olivedrab3", labels = TRUE)
+hist(resid(ek_irrad_model_ulva))
+plot(resid(ek_irrad_model_ulva) ~ fitted(ek_irrad_model_ulva))
+qqnorm(resid(ek_irrad_model_ulva))
+qqline(resid(ek_irrad_model_ulva))
+ulva %>% ggplot(aes(supersat_total)) +
         geom_histogram(binwidth=5, fill = "#5BB300", color = "black", size = 0.25, alpha = 0.85) +
         theme_bw()
 
-ulva %>% ggplot(aes(treatment_graph, irradiance_over_ek)) + 
-        geom_boxplot(size=0.5) + 
-        geom_point(alpha = 0.5, size = 3, aes(color = Temperature), show.legend = TRUE) + 
-        labs(x="Treatment (salinty/nitrate)", y= "Time above Ek (min)", title= "Time above Ek", subtitle = "Ulva lactuca") + 
-        scale_x_discrete(labels = c("35ppt/0.5umolN", "35ppt/14umolN", "28ppt/27umolN", "18ppt/53umolN", "11ppt/80umolN")) + 
-        stat_mean() + 
-        geom_hline(yintercept=0, color = "orange", size = 0.5, alpha = 0.5) +
-        scale_color_manual(values = c("#999999", "#E69F00", "#56B4E9")) +
-        theme_bw()
-
-
-
-#HYPNEA 
-ek_irrad_model_noint_hypnea <- lmer(formula = irradiance_over_ek ~ Treatment + Temperature + (1 | Run) + (1 | Plant.ID), data = hypnea)
-
-#make a histogram and residual plots of the data for ulva
-hist(hypnea$irradiance_over_ek, main = paste("Hypnea musciformis"), col = "maroon", labels = TRUE)
-hist(resid(ek_irrad_model_noint))
-plot(resid(ek_irrad_model_noint) ~ fitted(ek_irrad_model_noint))
-qqnorm(resid(ek_irrad_model_noint))
-qqline(resid(ek_irrad_model_noint))
-
-
 #check the performance of the model
-performance::check_model(ek_irrad_model_noint)
+performance::check_model(ek_irrad_model_ulva)
 
-r.squaredGLMM(ek_irrad_model_noint)
-summary(ek_irrad_model_noint)
+r.squaredGLMM(ek_irrad_model_ulva)
+summary(ek_irrad_model_ulva)
 
 #check for equal variance
-bartlett.test(irradiance_over_ek ~ Treatment, data = hypnea)
+bartlett.test(supersat_total ~ Treatment, data = ulva)
 #run Welch's ANOVA if not equal variance
-welch_anova_treatment <- oneway.test(irradiance_over_ek ~ Treatment, data = hypnea, var.equal = FALSE)
-welch_anova_treatment
-welch_anova_temp <- oneway.test(irradiance_over_ek ~ Temperature, data = hypnea, var.equal = FALSE)
-welch_anova_temp
-games_howell_test(hypnea, irradiance_over_ek ~ Treatment, conf.level = 0.95, detailed = TRUE)
-games_howell_test(hypnea, irradiance_over_ek ~ Temperature, conf.level = 0.95, detailed = TRUE)
+welch_anova_treatment_ulva <- oneway.test(supersat_total ~ Treatment, data = ulva, var.equal = FALSE)
+welch_anova_treatment_ulva
+welch_anova_temp_ulva <- oneway.test(supersat_total ~ Temperature, data = ulva, var.equal = FALSE)
+welch_anova_temp_ulva
+games_howell_test(ulva, supersat_total ~ Treatment, conf.level = 0.95, detailed = TRUE)
 
-plot(allEffects(ek_irrad_model_noint_hypnea))
+#run ANOVA and pairwise comparisons
+#anova(ek_irrad_model_noint, type = c("III"), ddf = "Satterthwaite")
+#ulva_ek_irrad_model_aov <- aov(supersat_total ~ Treatment + Temperature, data = ulva)
+#TukeyHSD(ulva_ek_irrad_model_aov, "Treatment", ordered = FALSE)
+#TukeyHSD(ulva_ek_irrad_model_aov, "Temperature", ordered = FALSE)
+
+plot(allEffects(ek_irrad_model_ulva))
+
+ulva %>% ggplot(aes(treatment_graph, supersat_total)) + 
+        geom_boxplot(size=0.5) + 
+        geom_point(alpha = 0.5, size = 3, aes(color = Temperature), show.legend = FALSE) + 
+        labs(x="Treatment (salinty/nitrate)", y= "Mean Daily Saturation Time (min)", title= "A", subtitle = "Ulva lactuca") + 
+        scale_x_discrete(labels = c("35ppt/0.5umolN", "35ppt/14umolN", "28ppt/27umolN", "28ppt/53umolN", "18ppt/53umolN", "11ppt/80umolN")) + 
+        ylim(100, 600) + stat_mean() + 
+        geom_hline(yintercept=100, color = "orange", size = 0.5, alpha = 0.5) +
+        scale_color_manual(values = c("#999999", "#E69F00", "#56B4E9")) +
+        theme_bw() +
+        theme(legend.position = c(0.90,0.90), plot.title = element_text(face = "bold", vjust = -15, hjust = 0.05), 
+              plot.subtitle = element_text(face = "italic", vjust = -20, hjust = 0.05))
+
+#summarize the means for rETRmax
+ulva %>% group_by(Treatment) %>% summarise_at(vars(supersat_total), list(mean = mean))
+ulva %>% group_by(Run) %>% summarise_at(vars(supersat_total), list(mean = mean))
+
 
 #plot a regression between the photosynthetic independent variables of interest and growth rate
-hypnea_growth_irrad_ek_graph <- ggplot(hypnea, aes(x=irradiance_over_ek, y=growth_rate)) + 
+ulva_growth_irrad_ek_graph <- ggplot(ulva, aes(x=supersat_total, y=growth_rate)) + 
         geom_point(alpha = 0.5, size = 3, aes(color = Treatment), show.legend = TRUE) + 
         geom_smooth(method = "lm", col = "black") + theme_bw() + 
-        labs(title = "Hypnea musciformis Minutes above Ek vs Growth Rate", x = "Minutes above Ek", y = "growth rate (%)") + 
-        stat_regline_equation(label.y = 155) + stat_cor(label.y = 145)
-hypnea_growth_irrad_ek_graph
+        labs(title = "A", subtitle = "Ulva lactuca", x = "Mean saturation time (min)", 
+             y = "growth rate (%)") + stat_regline_equation(label.x = 475, label.y = 225) + 
+        stat_cor(label.x = 475, label.y = 215) +
+        theme(plot.title = element_text(face = "bold", vjust = -15, hjust = 0.05), 
+              plot.subtitle = element_text(face = "italic", vjust = -20, hjust = 0.05))
+ulva_growth_irrad_ek_graph
 
-hypnea %>% ggplot(aes(irradiance_over_ek)) +
+#HYPNEA
+#_____________________________________________________________________________________________________________
+ek_irrad_model_hypnea <- lmer(formula = supersat_total ~ Treatment + Temperature + (1 | Run) + (1 | Plant.ID), data = hypnea)
+
+#make a histogram and residual plots of the data for ulva
+hist(hypnea$supersat_total, main = paste("Hypnea musciformis"), col = "maroon", labels = TRUE)
+hist(resid(ek_irrad_model_hypnea))
+plot(resid(ek_irrad_model_hypnea) ~ fitted(ek_irrad_model_hypnea))
+qqnorm(resid(ek_irrad_model_hypnea))
+qqline(resid(ek_irrad_model_hypnea))
+hypnea %>% ggplot(aes(supersat_total)) +
         geom_histogram(binwidth=5, fill = "#a8325e", color = "black", size = 0.25, alpha = 0.85) +
         theme_bw()
 
-hypnea %>% ggplot(aes(treatment_graph, irradiance_over_ek)) + 
+#check the performance of the model
+performance::check_model(ek_irrad_model_hypnea)
+
+r.squaredGLMM(ek_irrad_model_hypnea)
+summary(ek_irrad_model_hypnea)
+
+#check for equal variance
+bartlett.test(supersat_total ~ Treatment, data = hypnea)
+#run Welch's ANOVA if not equal variance
+welch_anova_treatment <- oneway.test(supersat_total ~ Treatment, data = hypnea, var.equal = FALSE)
+welch_anova_treatment
+welch_anova_temp <- oneway.test(supersat_total ~ Temperature, data = hypnea, var.equal = FALSE)
+welch_anova_temp
+games_howell_test(hypnea, supersat_total ~ Treatment, conf.level = 0.95, detailed = TRUE)
+games_howell_test(hypnea, supersat_total ~ Temperature, conf.level = 0.95, detailed = TRUE)
+
+plot(allEffects(ek_irrad_model_hypnea))
+
+hypnea %>% ggplot(aes(treatment_graph, supersat_total)) + 
         geom_boxplot(size=0.5) + 
         geom_point(alpha = 0.5, size = 3, aes(color = Temperature), show.legend = TRUE) + 
-        labs(x="Treatment (salinty/nitrate)", y= "Time above Ek (min)", title= "Time above Ek", subtitle = "Hypnea musciformis") + 
-        scale_x_discrete(labels = c("35ppt/14umolN", "28ppt/27umolN", "28ppt/53umolN", "18ppt/53umolN", "11ppt/80umolN")) + 
-        stat_mean() + 
-        geom_hline(yintercept=0, color = "orange", size = 0.5, alpha = 0.5) +
+        labs(x="Treatment (salinty/nitrate)", y= "Mean Daily Saturation Time (min)", title= "B", subtitle = "Hypnea musciformis") + 
+        scale_x_discrete(labels = c("35ppt/0.5umolN", "35ppt/14umolN", "28ppt/27umolN", "28ppt/53umolN", "18ppt/53umolN", "11ppt/80umolN")) + 
+        ylim(100, 600) + stat_mean() + 
+        geom_hline(yintercept=100, color = "orange", size = 0.5, alpha = 0.5) +
         scale_color_manual(values = c("#999999", "#E69F00", "#56B4E9")) +
-        theme_bw()
+        theme_bw() +
+        theme(legend.position = c(0.88,0.88), plot.title = element_text(face = "bold", vjust = -15, hjust = 0.05), 
+              plot.subtitle = element_text(face = "italic", vjust = -20, hjust = 0.05))
+
+#plot a regression between the photosynthetic independent variables of interest and growth rate
+hypnea_growth_irrad_ek_graph <- ggplot(hypnea, aes(x=supersat_total, y=growth_rate)) + 
+        geom_point(alpha = 0.5, size = 3, aes(color = Treatment), show.legend = TRUE) + 
+        geom_smooth(method = "lm", col = "black") + theme_bw() + 
+        labs(title = "B", subtitle = "Hypnea musciformis", x = "Mean saturation time (min)", y = "growth rate (%)") + 
+        stat_regline_equation(label.x = 450, label.y = 150) + stat_cor(label.x = 450, label.y = 140) +
+        theme(plot.title = element_text(face = "bold", vjust = -15, hjust = 0.05), 
+              plot.subtitle = element_text(face = "italic", vjust = -20, hjust = 0.05))
+hypnea_growth_irrad_ek_graph
 
